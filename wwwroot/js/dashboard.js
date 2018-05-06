587abd7074f9382e2bdae3c891b6c63d05ece0aa
danielev8 <danielevalberg@gmail.com>
2018-05-06 09:27:48 +0000

    admin stuff again

--- wwwroot/js/dashboard.js
+++ wwwroot/js/dashboard.js
@@ -5,3 +5,34 @@ $(".button-collapse").sideNav();
 $(document).ready(function() {
     $('.mdb-select').material_select();
   });
+
+
+function checkBoxCollapse(checkboxId, collapseDivId) {
+
+  if ($("#" + checkboxId).is(":checked")) {
+    $("#" + collapseDivId).slideDown();
+  } else {
+    $("#" + collapseDivId).slideUp();
+  }
+
+}
+
+// Make Paperback collapse
+$("#showPaperback").click( function() {
+  checkBoxCollapse("includePaperback", "paperback");
+});
+
+// Make Hardcover collapse 
+$("#showHardcover").click( function() {
+  checkBoxCollapse("includeHardcover", "hardcover");
+});
+
+// Make e-book collapse
+$("#showEbook").click( function() {
+  checkBoxCollapse("includeEbook", "ebook");
+});
+
+// Make audio book collapse 
+$("#showAudioBook").click( function() {
+  checkBoxCollapse("includeAudioBook", "audioBook");
+});
