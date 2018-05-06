11c8b6c20a75e5864f8764b2c1cf840028f0339a
JonniPolar <jonj16@ru.is>
2018-05-06 17:04:26 +0000

    setti inn authentication fyrir users sem virkar bæði i signup og login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BookCave.Models;
 using BookCave.Models.InputModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +11,11 @@ namespace BookCave.Controllers
     public class UserController : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly UserManager<ApplicationUser> _userManger;
+        private readonly UserManager<ApplicationUser> _userManager;
         public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
-            _userManger = userManager;
+            _userManager = userManager;
         }
         [HttpGet]
         public IActionResult Login()
@@ -22,8 +24,18 @@ namespace BookCave.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(UserLoginInputModel user)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(UserLoginInputModel model)
         {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
         [HttpGet]
@@ -32,20 +44,26 @@ namespace BookCave.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignUp(UserSignUpInputModel model)
         {
-           /* if(!ModelState.IsValid) 
+            if(!ModelState.IsValid) 
             {
                 return View();
             }
+
             var user = new ApplicationUser { UserName = model.Email, Email = model.Email};
 
-            var result = await _userManger.CreateAsync(user, model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
 
             if(result.Succeeded)
             {
-                return View();
-            }*/
+                await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
+                await _signInManager.SignInAsync(user, false);
+                await _userManager.AddToRoleAsync(user, "User");
+
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
         public IActionResult AccountInformation()
@@ -72,6 +90,7 @@ namespace BookCave.Controllers
         {
 
         }
+        [Authorize]
         public IActionResult OrderHistory()
         {
             return View();
