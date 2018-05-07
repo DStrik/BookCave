5b50148e99febc81dfa71ba8702bff04104510f2
ViktorSveins <viktorsveins@gmail.com>
2018-05-07 16:33:13 +0000

    grammarfix

---
+++ Migrations/20180507162900_grammarfix.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class grammarfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookTypeId",
+                table: "Stocks",
+                newName: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "Stocks",
+                newName: "BookTypeId");
+        }
+    }
+}
