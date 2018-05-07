9faa27adaad08c02dafba8b79436e34f24ca62e9
ViktorSveins <viktorsveins@gmail.com>
2018-05-07 13:17:05 +0000

    refactored code in  bookrepo

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,11 +1,17 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
+using BookCave.Services;
 
 namespace BookCave.Controllers
 {
     public class BookController : Controller
     {
+        private BookService _bookService;
+        public BookController()
+        {
+            _bookService = new BookService();
+        }
         public IActionResult Top10()
         {
             return View();
