7f50ab7bd40809398adfd629517bf1f7fa5a5ee8
Megg <arnimegg@gmail.com>
2018-05-08 12:30:59 +0000

    cart cart cart

--- wwwroot/js/CartControllerJQ.js
+++ wwwroot/js/CartControllerJQ.js
@@ -1,13 +1,22 @@
-$('#incrementQty').click( function() {
-    var counter = $('#qty').val();
-    counter++ ;
-    $('#qty').val(counter);
-});
-$('#decrementQty').click( function() {
-    var counter = $('#qty').val();
-    if(counter > 1){
-    counter-- ;
-    $('#qty').val(counter);
-    }
+$(document).ready(function(){
+    console.log("1");
+    $(".incrementQty").click( function() {
+        var counter = $('#qty').val();
+        console.log("hey");
+        counter++ ;
+        $('#qty').val(counter);
+        
+        
+    });
+    $(".decrementQty").click( function() {
+        var counter = $('#qty').val();
+        if(counter > 1){
+        counter-- ;
+        $('#qty').val(counter);
+        }
 
+    });
+    $('#gaur').click(function(){
+        console.log("hallo");
+    });
 });
