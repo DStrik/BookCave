fac127d38baa37cb0612149dc726790aa1456d94
ViktorSveins <viktorsveins@gmail.com>
2018-05-10 17:53:27 +0000

    migration!

---
+++ Migrations/20180510175213_reviewer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class reviewer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "BookReviews",
+                newName: "Reviewer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Reviewer",
+                table: "BookReviews",
+                newName: "UserId");
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.Id);
+                });
+        }
+    }
+}
