aa28425e3bf3534c0d2b6eef33aba1d0d3626c7a
Danielev8 <danielevalberg@gmail.com>
2018-05-10 18:59:43 +0000

    fixed js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,15 @@
 ﻿new WOW().init();
 
-
-
 $(document).ready(function(){
-    $(".rate").rate();
+    // Initiate star ratings
+    var options = {
+        max_value: 5,
+        step_size: 0.5,
+    }
+    $(".rate").rate(options);
+
+    alert('off!');
+    $('.rate-off').off(function() {
+        alert("off!");
+    });
 });
