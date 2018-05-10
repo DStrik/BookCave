68ed83f56e4a8de595098a34d9e164d6874fca18
JonniPolar <jonj16@ru.is>
2018-05-10 17:24:26 +0000

    Checkout alveg að fara að lúkka vel

--- wwwroot/js/CheckOutControllerJQ.js
+++ wwwroot/js/CheckOutControllerJQ.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
     var navListItems = $('div.setup-panel-2 div a'),
         allWells = $('.setup-content-2'),
         allNextBtn = $('.nextBtn-2'),
-        allPrevBtn = $('.prevBtn-2');
+        allPrevBtn = $('.prevBtn-2'),
+        lastNavStep = currNavStep = $('div.setup-panel-2 div a[href="#step-3"]');
 
     allWells.hide();
 
@@ -16,13 +17,23 @@ $(document).ready(function () {
         e.preventDefault();
         var $target = $($(this).attr('href')),
             $item = $(this);
+            nextStep = $item.parent().next().children("a")
 
         if (!$item.hasClass('disabled')) {
             navListItems.removeClass('btn-amber').addClass('btn-success');
-            $item.removeClass('btn-blue-gray').addClass('btn-amber');
+            $item.removeClass('btn-blue-grey').addClass('btn-amber');
             allWells.hide();
             $target.show();
             $target.find('input:eq(0)').focus();
+        }
+
+        if(!lastNavStep.hasClass('btn-success')){
+
+            while(!nextStep.hasClass('disabled')){
+                nextStep.addClass('disabled');
+                nextStep.removeClass('btn-success').addClass('btn-blue-grey');
+                nextStep = nextStep.parent().next.children;
+            }
         }
     });
 
@@ -30,42 +41,80 @@ $(document).ready(function () {
         var curStep = $(this).closest(".setup-content-2"),
             curStepBtn = curStep.attr("id"),
             prevStepSteps = $('div.setup-panel-2 div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+            currNavStep = $('div.setup-panel-2 div a[href="#' + curStepBtn + '"]');
 
         prevStepSteps.removeAttr('disabled').trigger('click');
+    
     });
 
     allNextBtn.click(function () {
+
         var curStep = $(this).closest(".setup-content-2"),
             curStepBtn = curStep.attr("id"),
             nextStepSteps = $('div.setup-panel-2 div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            currNavStep = $('div.setup-panel-2 div a[href="#' + curStepBtn + '"]');
             curInputs = curStep.find("input[type='text'],input[type='url']"),
             isValid = true;
 
-        $(".form-group").removeClass("has-error");
-        for (var i = 0; i < curInputs.length; i++) {
-            if (!curInputs[i].validity.valid) {
-                isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
-            }
-        }
+        //verifies input in step on before going to next step
+        //also fills in the input feild in step 2 as a read only
+        if($(this).attr('id') == "verifyStep"){
 
-        if (isValid){
 
-            nextStepSteps.removeAttr('disabled').trigger('click');
+            var bla = $("#checkOutForm").serialize();
+            $.get('/CheckOut/Verify', bla, function(data, status) {
+
+                $("#BillingFirstName1").val($("#BillingFirstName").val());
+                $("#BillingLastName1").val($("#BillingLastName").val());
+                $("#BillingStreetName1").val($("#BillingStreetName").val());
+                $("#BillingHouseNumber1").val($("#BillingHouseNumber").val());
+                $("#BillingCity1").val($("#BillingCity").val());
+                $("#BillingZipCode1").val($("#BillingZipCode").val());
+                $("#BillingCountry1").val($("#BillingCountry").val());
+                $("#ShippingFirstName1").val($("#ShippingFirstName").val());
+                $("#ShippingLastName1").val($("#ShippingLastName").val());
+                $("#ShippingStreetName1").val($("#ShippingStreetName").val());
+                $("#ShippingHouseNumber1").val($("#ShippingHouseNumber").val());
+                $("#ShippingCity1").val($("#ShippingCity").val());
+                $("#ShippingZipCode1").val($("#ShippingZipCode").val());
+                $("#ShippingCountry1").val($("#ShippingCountry").val());
+                $("#FullName1").val($("#FullName").val());
+                $("#CardNumber1").val($("#CardNumber").val());
+                $("#ExpirationMonth1").val($("#ExpirationMonth").val());
+                $("#ExpirationYear1").val($("#ExpirationYear").val());
+                $("#Cvc1").val($("#Cvc").val());
+
+                $(".form-group").removeClass("has-error");
+                for (var i = 0; i < curInputs.length; i++) {
+                    if (!curInputs[i].validity.valid) {
+                        isValid = false;
+                        $(curInputs[i]).closest(".form-group").addClass("has-error");
+                    }
+                }
+
+                if (isValid){
+
+                    nextStepSteps.removeClass('disabled').trigger('click');
+                }
+
+            }).fail(function(err) {
+                alert("something wrong!")
+            });
+        };
+
+        if($(this).attr('id') == "pay") {
+
+            $("#pay").one('click', function(){
+                var bla = $("#checkOutForm").serialize();
+                $.post('/CheckOut/Pay', bla, function(data, status) {
+                }).fail(function(err) {
+                    alert("Error has occured!")
+                });
             
+            });
         }
-        /*
-        var target = nextStepSteps.attr('href');
-
-        if (!nextStepSteps.hasClass('disabled')) {
-            navListItems.removeClass('btn-amber').addClass('btn-success');
-            nextStepSteps.addClass('btn-amber');
-            allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
-        }
-        */
     });
+
 
     $('div.setup-panel-2 div a.btn-amber').trigger('click');
 
@@ -82,47 +131,7 @@ $(document).ready(function () {
         alert("Error has occured!")
     });
 
-    //verifies input in step on before going to next step
-    //also fills in the input feild in step 2 as a read only
-    $("#verifyStep").click(function(){
-        var bla = $("#checkOutForm").serialize();
-        $.get('/CheckOut/Verify', bla, function(data, status) {
-
-            $("#BillingFirstName1").val($("#BillingFirstName").val());
-            $("#BillingLastName1").val($("#BillingLastName").val());
-            $("#BillingStreetName1").val($("#BillingStreetName").val());
-            $("#BillingHouseNumber1").val($("#BillingHouseNumber").val());
-            $("#BillingCity1").val($("#BillingCity").val());
-            $("#BillingZipCode1").val($("#BillingZipCode").val());
-            $("#BillingCountry1").val($("#BillingCountry").val());
-            $("#ShippingFirstName1").val($("#ShippingFirstName").val());
-            $("#ShippingLastName1").val($("#ShippingLastName").val());
-            $("#ShippingStreetName1").val($("#ShippingStreetName").val());
-            $("#ShippingHouseNumber1").val($("#ShippingHouseNumber").val());
-            $("#ShippingCity1").val($("#ShippingCity").val());
-            $("#ShippingZipCode1").val($("#ShippingZipCode").val());
-            $("#ShippingCountry1").val($("#ShippingCountry").val());
-            $("#FullName1").val($("#FullName").val());
-            $("#CardNumber1").val($("#CardNumber").val());
-            $("#ExpirationMonth1").val($("#ExpirationMonth").val());
-            $("#ExpirationYear1").val($("#ExpirationYear").val());
-            $("#Cvc1").val($("#Cvc").val());
-
-
-        }).fail(function(err) {
-            alert("something wrong!")
-        });
-
-        $("#pay").one('click', function(){
-            var bla = $("#checkOutForm").serialize();
-            $.post('/CheckOut/Pay', bla, function(data, status) {
-            }).fail(function(err) {
-                alert("Error has occured!")
-            });
-        
-        });
     
-    });
 
     // Changes the status of the input boxes in billing so that if checked
     // the input is disabled and information from shipping is copied to
