0e534f33e641afd1a2f94672ba6ce2f01dd7d991
DanniB <dannimb@gmail.com>
2018-05-11 17:42:06 +0000

    Landing page ready, as well as the navbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookCave.Models;
 using BookCave.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace BookCave.Controllers
 {
     public class HomeController : Controller
     {
         private BookService _bookService;
+        private UserService _userService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController()
+        public HomeController(UserManager<ApplicationUser> userManager)
         {
             _bookService = new BookService();
+            _userService = new UserService();
+            _userManager = userManager;
         }
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if(user == null)
+            {
+                return View(); // Should go to error page here
+            }
+
+            var model = _userService.GetUserImage(user.Id);
+
+            if(model == null)
+            {
+                return View(); // Should go to error page here
+            }
+
+            return View(model);
         }
 
         public IActionResult Error()
